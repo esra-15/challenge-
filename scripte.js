2155abfe407337a99aec1524548ef077dff33edb
Esra hamed <s2212176087@ku.edu.kw>
2025-08-28 19:39:58 +0300

    ..

---
+++ scripte.js
@@ -0,0 +1,11 @@
+let books =[book1, book2, book3]
+push
+
+let name2 = prompt("what is your name ?");
+let age2 = prompt("what is your age ?");
+
+
+
+alert("HELLO " + name2 + " your age is " + age2 );
+
+console.log( "%c age name city" ,"color: green");
